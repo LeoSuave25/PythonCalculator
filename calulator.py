23a721bafc11003455836422b022de05f4db316b
LeoSuave25 <suaverdez90@gmail.com>
2023-05-09 21:45:18 +0800

    Pseudocode Done

---
+++ calulator.py
@@ -0,0 +1,8 @@
+#Python Calculator
+#Ask the user to choose one of the four math operations (Addition, Subtraction, Multiplication and Division)
+#Ask the user for two numbers
+#Display the result
+#Ask the user if the users wants to try again or not
+#If yes, repeat Step 1
+#If no, display thank you
+#Use Python Function and appropriate Exceptions to capture errors during runtime.
