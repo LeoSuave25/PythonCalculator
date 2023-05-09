bc17b96d3e2e1f783e4e07101ffb0ce78a143ec2
LeoSuave25 <suaverdez90@gmail.com>
2023-05-09 23:54:25 +0800

    Styling Done

--- calulator.py
+++ calulator.py
@@ -1,4 +1,9 @@
 #Python Calculator
+import pyfiglet as fig
+print("\033[32m","="*100,"\033[m")
+title = "Python Calculator"
+print("\033[32m",fig.figlet_format(title),"\033[m")
+print(fig.figlet_format("Made by: Leoj M Suaverdez",font="bubble"))
 #Operation Methods
 def add(first, second):
     return first + second
@@ -16,7 +21,7 @@ def get_number_input(prompt):
         try:
             return float(input(prompt))
         except ValueError:
-            print("Invalid input! Please enter a valid number.")
+            print("\033[1;31mInvalid input! Please enter a valid number.\033[0m")
 #Ask the user to choose one of the four math operations (Addition, Subtraction, Multiplication and Division)
 operations = {
     1: ("Addition", add),
@@ -34,20 +39,20 @@ while response == "Y":
         try:
             operation = int(input("Enter 1 | 2 | 3 | 4: "))
             if operation not in operations:
-                raise ValueError("Invalid operation! Please enter a valid operation number (1-4).")
+                raise ValueError("\033[1;31mInvalid operation! Please enter a valid operation number (1-4).\033[0m")
             break
         except ValueError as error:
                 print(error)
 
     #Ask the user for two numbers
-    print(f"You are now using {operations[operation][0]}")
+    print(f"You are now using \033[1;32m{operations[operation][0]}\033[0m")
     first_number = get_number_input("Enter the first number: ")
     second_number = get_number_input("Enter the second number: ")
     try:
         _, operation_function = operations[operation]
         result = operation_function(first_number, second_number)
     except ZeroDivisionError:
-        print("Error: division by zero!")
+        print("\033[1;31mError: division by zero!\033[0m")
         continue
     #Display the result
     print("Result:", result)
@@ -57,10 +62,11 @@ while response == "Y":
         try:
             response = input("Do you want to try again? Y/N: ").upper()
             if response not in {"Y","N"}:
-                raise ValueError("Invalid Response! Please enter a valid Letter.")
+                raise ValueError("\033[1;31mInvalid Response! Please enter a valid Letter.\033[0m")
             break
         except ValueError as error:
                 print(error)
 #If no, display thank you
-print("Thank You!")
+print("\033[1;32mThank You!\033[0m")
+print("\033[32m","="*100,"\033[m")
 #Use Python Function and appropriate Exceptions to capture errors during runtime.
